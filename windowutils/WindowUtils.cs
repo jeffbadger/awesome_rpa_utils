745eccd6f711d1587d39d77b6ba8053e9be114c6
Jeff Badger <jbadger110@gmail.com>
2026-08-26 23:27:14 -0400

    Add ShowWindowCommand enum and Win32 interop layer to WindowUtils

--- windowutils/WindowUtils.cs
+++ windowutils/WindowUtils.cs
@@ -1,7 +1,30 @@
+using System;
+using System.Collections.Generic;
 using System.ComponentModel;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
 
 namespace WindowAutomation
 {
+    /// <summary>
+    /// The window-state command applied by <see cref="WindowUtils.SetWindowState"/>,
+    /// wrapping the Win32 <c>SW_*</c> constants used by <c>ShowWindow</c>.
+    /// </summary>
+    public enum ShowWindowCommand
+    {
+        /// <summary>Hides the window (SW_HIDE, 0).</summary>
+        Hide = 0,
+        /// <summary>Shows the window in its normal (restored) state (SW_SHOWNORMAL, 1).</summary>
+        Normal = 1,
+        /// <summary>Shows the window maximized (SW_SHOWMAXIMIZED, 3).</summary>
+        Maximized = 3,
+        /// <summary>Minimizes the window without activating another window (SW_MINIMIZE, 6).</summary>
+        Minimized = 6,
+        /// <summary>Restores a minimized/maximized window to its previous size and position (SW_RESTORE, 9).</summary>
+        Restore = 9
+    }
+
     /// <summary>
     /// Pega Robot Studio-ready component that enumerates, locates, moves/resizes,
     /// activates, and closes windows using the Win32 window APIs.
@@ -25,5 +48,87 @@ namespace WindowAutomation
         {
             container?.Add(this);
         }
+
+        #region Win32 Interop
+
+        private const uint WM_CLOSE = 0x0010;
+
+        private static readonly IntPtr HWND_TOPMOST = new IntPtr(-1);
+        private static readonly IntPtr HWND_NOTOPMOST = new IntPtr(-2);
+        private const uint SWP_NOSIZE = 0x0001;
+        private const uint SWP_NOMOVE = 0x0002;
+        private const uint SWP_NOACTIVATE = 0x0010;
+
+        private delegate bool EnumWindowsProc(IntPtr hWnd, IntPtr lParam);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern bool EnumWindows(EnumWindowsProc lpEnumFunc, IntPtr lParam);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern bool EnumChildWindows(IntPtr hWndParent, EnumWindowsProc lpEnumFunc, IntPtr lParam);
+
+        [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Unicode)]
+        private static extern IntPtr FindWindowNative(string lpClassName, string lpWindowName);
+
+        [DllImport("user32.dll", CharSet = CharSet.Unicode)]
+        private static extern int GetWindowTextLength(IntPtr hWnd);
+
+        [DllImport("user32.dll", CharSet = CharSet.Unicode)]
+        private static extern int GetWindowText(IntPtr hWnd, StringBuilder lpString, int nMaxCount);
+
+        [DllImport("user32.dll", CharSet = CharSet.Unicode)]
+        private static extern int GetClassName(IntPtr hWnd, StringBuilder lpClassName, int nMaxCount);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern uint GetWindowThreadProcessId(IntPtr hWnd, out uint lpdwProcessId);
+
+        [DllImport("user32.dll", EntryPoint = "GetForegroundWindow")]
+        private static extern IntPtr GetForegroundWindowNative();
+
+        [DllImport("user32.dll", EntryPoint = "SetForegroundWindow", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool SetForegroundWindowNative(IntPtr hWnd);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern bool GetWindowRect(IntPtr hWnd, out RECT lpRect);
+
+        [DllImport("user32.dll", EntryPoint = "MoveWindow", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool MoveWindowNative(IntPtr hWnd, int X, int Y, int nWidth, int nHeight, [MarshalAs(UnmanagedType.Bool)] bool bRepaint);
+
+        [DllImport("user32.dll", EntryPoint = "ShowWindow")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool ShowWindowNative(IntPtr hWnd, int nCmdShow);
+
+        [DllImport("user32.dll", EntryPoint = "IsWindowVisible")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool IsWindowVisibleNative(IntPtr hWnd);
+
+        [DllImport("user32.dll", EntryPoint = "IsWindow")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool IsWindowNative(IntPtr hWnd);
+
+        [DllImport("user32.dll", EntryPoint = "IsHungAppWindow")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool IsHungAppWindowNative(IntPtr hWnd);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool PostMessage(IntPtr hWnd, uint Msg, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int X, int Y, int cx, int cy, uint uFlags);
+
+        [StructLayout(LayoutKind.Sequential)]
+        private struct RECT
+        {
+            public int Left;
+            public int Top;
+            public int Right;
+            public int Bottom;
+        }
+
+        #endregion
     }
 }
